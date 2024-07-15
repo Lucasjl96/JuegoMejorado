332dac46fe418e3a8d8f08543f2cccc28599b9d7
Lucas Lamenza <123519419+Lucasjl96@users.noreply.github.com>
2024-07-15 19:05:38 -0300

    Update app.js

--- Proyecto2/app.js
+++ Proyecto2/app.js
@@ -1,8 +1,4 @@
-//lo definimos en una variable para saber bien a que elemento nos estamos refiriendo
 
-
-//mas avanzado en las clases se ha eliminado la declaracion de variables
-// y se realizo todo en una funcion para luego llamarlas en dos veces y achicar el codigo
 
 //------------------------------------------------------
 let numeroSecreto = 0;
@@ -19,12 +15,11 @@ function verificarIntento() {
     console.log(intentos);
     if(numeroDeUsuario === numeroSecreto){
         asignarTextoElemento('p',`Acertaste el numero en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`); 
-        //vamos a agregar codigo, se va a habilitar el boton desabilitado de nuevo juego, una vez
-        //que el usuario acierte.
+ 
         document.getElementById('reiniciar').removeAttribute('disabled');   
     } 
     else{
-        //esto es para cuando el usuario no acerto
+        
         if(numeroDeUsuario > numeroSecreto){
             asignarTextoElemento('p', 'El numero secreto es menor');
         }
@@ -36,9 +31,7 @@ function verificarIntento() {
     }
     return;
 };
-//aqui hemos creado una funcion que se utilizara para limpiar la caja de intentos
-//y luego volveremos a llamar a la funcion en la parte del codigo donde esta lo que sucedera
-// cuando el usuario no acierta.
+
 function limpiarCaja(){
     document.querySelector('#valorUsuario').value = '';
 };
@@ -55,15 +48,11 @@ function condicionesIniciales(){
 };
 
 function reiniciarJuego(){
-    //primero va a limpiar la caja
+    
     limpiarCaja();
-    //Indicar de nuevo el mensaje del inicio
-    
-    //generar un nuevo numero aleatorio
-    
-    //inicializar el numero de intentos
+   
     condicionesIniciales();
-    //dejar nuevamente el segundo boton desabilitado
+  
     document.querySelector('#reiniciar').setAttribute('disabled','true');
     
 };
